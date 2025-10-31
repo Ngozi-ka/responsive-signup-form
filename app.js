f98a527b26793bf2ac1056bc4dae01893b0d459c
Ngozi-ka <nwekengozika5@gmail.com>
2025-10-31 12:13:00 +0100

    project completed

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const form = document.querySelector(".form");
+
 const btn = document.querySelector(".btn");
 const firstname = document.querySelector(".firstname");
 const lastname = document.querySelector(".lastname");
@@ -15,82 +15,36 @@ const thirdImg = document.querySelector(".third-img");
 const fourthImg = document.querySelector(".fourth-img");
 
 btn.addEventListener("click", function () {
-  //   e.preventDefault();
-
-  // inputs.find(function(input){
-  //     return input === "";
-  // })
-
-  //   if (firstname.value === "" && lastname.value === "" && email.value === "" && password.value === ""){
-  //     firstnameError.textContent = "First name cannot be empty";
-  //     firstImg.style.opacity = 100;
-  //     lastnameError.textContent = "Last name cannot be empty";
-  //     secondImg.style.opacity = 100;
-  //     emailError.textContent = "Looks like this is not an email";
-  //     thirdImg.style.opacity = 100;
-  //     passwordError.textContent = "Password cannot be empty";
-  //     fourthImg.style.opacity = 100;
-  //  } else if (firstname.value === ""){
-  //     firstnameError.textContent = "First name cannot be empty";
-  //     firstImg.style.opacity = 100;
-  //     if(firstname.value !== ""){
-  //         firstnameError.textContent = "";
-  //     firstImg.style.opacity = 0;
-  //     }
-  // }
-  //   else if (lastname.value === "") {
-  //     lastnameError.textContent = "Last name cannot be empty";
-  //     secondImg.style.opacity = 100;
-  //   } else if (email.value === "") {
-  //     emailError.textContent = "Looks like this is not an email";
-  //     thirdImg.style.opacity = 100;
-  //   } else if (password.value === "") {
-  //     passwordError.textContent = "Password cannot be empty";
-  //     fourthImg.style.opacity = 100;
-  //   }else{
-  //     form.submit = true;
-  //   }
-
-  // if (firstname.value === ""){
-  //     firstnameError.textContent = "First name cannot be empty";
-  //     firstImg.style.opacity = 100;
-  //     if(firstname.value !== ""){
-  //         firstnameError.textContent = "";
-  //     firstImg.style.opacity = 0;
-  //     }
-  // }
-  //   else if (lastname.value === "") {
-  //     lastnameError.textContent = "Last name cannot be empty";
-  //     secondImg.style.opacity = 100;
-  //   } else if (email.value === "") {
-  //     emailError.textContent = "Looks like this is not an email";
-  //     thirdImg.style.opacity = 100;
-  //   } else if (password.value === "") {
-  //     passwordError.textContent = "Password cannot be empty";
-  //     fourthImg.style.opacity = 100;
-  //   }else{
-  //     form.submit = true;
-  //   }
 
   firstname.value === ""
     ? ((firstnameError.textContent = "First name cannot be empty"),
-      (firstImg.style.opacity = 100))
-    : ((firstnameError.textContent = ""), (firstImg.style.opacity = 0));
+      (firstImg.style.opacity = 100),
+      (firstname.style.border = "1px solid hsl(0, 100%, 74%)"))
+    : ((firstnameError.textContent = ""),
+      (firstImg.style.opacity = 0),
+      (firstname.style.border = "1px solid hsla(246, 25%, 77%, 0.356)"));
 
   lastname.value === ""
     ? ((lastnameError.textContent = "Last name cannot be empty"),
-      (secondImg.style.opacity = 100))
-    : ((lastnameError.textContent = ""), (secondImg.style.opacity = 0));
+      (secondImg.style.opacity = 100),
+      (lastname.style.border = "1px solid hsl(0, 100%, 74%)"))
+    : ((lastnameError.textContent = ""),
+      (secondImg.style.opacity = 0),
+      (lastname.style.border = "1px solid hsla(246, 25%, 77%, 0.356)"));
 
-    email.value === ""? 
-   ( emailError.textContent = "Looks like this is not an email",
-   thirdImg.style.opacity = 100) :    ( emailError.textContent = "",
-   thirdImg.style.opacity = 0)
+  email.value === ""
+    ? ((emailError.textContent = "Looks like this is not an email"),
+      (thirdImg.style.opacity = 100),
+      (email.style.border = "1px solid hsl(0, 100%, 74%)"))
+    : ((emailError.textContent = ""),
+      (thirdImg.style.opacity = 0),
+      (email.style.border = "1px solid hsla(246, 25%, 77%, 0.356)"));
 
-   password.value === ""? 
-  ( passwordError.textContent = "Password cannot be empty",
-  fourthImg.style.opacity = 100) : ( passwordError.textContent = "",
-  fourthImg.style.opacity = 0)
-
+  password.value === ""
+    ? ((passwordError.textContent = "Password cannot be empty"),
+      (fourthImg.style.opacity = 100),
+      (password.style.border = "1px solid hsl(0, 100%, 74%)"))
+    : ((passwordError.textContent = ""),
+      (fourthImg.style.opacity = 0),
+      (password.style.border = "1px solid hsla(246, 25%, 77%, 0.356)"));
 });
-
